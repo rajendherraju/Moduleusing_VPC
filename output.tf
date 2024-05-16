0bfcbb916076cf23e194ceb23858796c07224c9f
rajendherraju <38126617+rajendherraju@users.noreply.github.com>
2024-05-16 10:51:54 +0530

    Update output.tf
    updated

--- output.tf
+++ output.tf
@@ -3,17 +3,32 @@ output "vpc_id" {
   value       = module.vpc.vpc_id
 }
 
-output "public_subnets" {
-  description = "List of public subnet IDs"
-  value       = module.vpc.public_subnets
+# VPC CIDR blocks
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = module.vpc.vpc_cidr_block
 }
 
+# VPC Private Subnets
 output "private_subnets" {
-  description = "List of private subnet IDs"
+  description = "A list of private subnets inside the VPC"
   value       = module.vpc.private_subnets
 }
 
-output "nat_gateway_ids" {
-  description = "List of NAT Gateway IDs"
-  value       = module.vpc.nat_gateway_ids
+# VPC Public Subnets
+output "public_subnets" {
+  description = "A list of public subnets inside the VPC"
+  value       = module.vpc.public_subnets
+}
+
+# VPC NAT gateway Public IP
+output "nat_public_ips" {
+  description = "List of public Elastic IPs created for AWS NAT Gateway"
+  value       = module.vpc.nat_public_ips
+}
+
+# VPC AZs
+output "azs" {
+  description = "A list of availability zones specified as argument to this module"
+  value       = module.vpc.azs
 }
